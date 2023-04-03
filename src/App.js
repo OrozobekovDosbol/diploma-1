5fec118aa4cec01e10d766e40a2c45279503a17b
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-04-03 15:43:49 +0600

    instal react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 function App() {
   return (
-    <div className="App">\
+    <div className="App">
       <h1>Hello world</h1>
+      
+      <h2>Bye</h2>
+      <img src="" />
     </div>
   );
 }
