7686ce7c9757d1378e49ac1ed72da0b4684da23d
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-04-07 14:04:02 +0600

    added new file Layout and Nav

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import Nav from "../Nav/Nav";
+import logo from "../../assets/logo.png";
+import "./Layout.css";
+
+export default function Layout(props) {
+  return (
+    <div className="Layout">
+      <header>
+        <img src={logo} alt="logo" className="logo" />
+        <Nav />
+      </header>
+      <aside>
+        <nav>NAV CATEGORIES</nav>
+      </aside>
+      <main>{props.children}</main>
+      <footer>FOOTER</footer>
+    </div>
+  );
+}
