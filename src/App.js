e26f9e2b39999a46deb230d70b9f1347bfa614a1
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-04-07 13:58:51 +0600

    Added a code that should trigger an error.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Link, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-      
-      <h2>Bye</h2>
-      <img src="" />
+      <Link to="/">Home</Link>
+      <Link to="/">Phones</Link>
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/phones" element={<h1>Phones</h1>} />
+      </Routes>
+
+
+      <h2>Home page</h2>
+      <h2>Phones page</h2>
     </div>
   );
 }
