4903cc01326e1410a96d8e4b3691d7ce5b4b60ef
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-04-24 14:22:26 +0600

    firebase imported GoogleProvider

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from 'firebase/firestore/lite'
+import { getFirestore, collection } from 'firebase/firestore/lite';
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,17 +14,21 @@ const firebaseConfig = {
   projectId: "diplom-1-b6b30",
   storageBucket: "diplom-1-b6b30.appspot.com",
   messagingSenderId: "62749632246",
-  appId: "1:62749632246:web:a591d53be89e0675c9aed7"
+  appId: "1:62749632246:web:a591d53be89e0675c9aed7",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
+export const categoriesCollection = collection(db, "categories");
 
+export const productsCollection = collection(db, "products");
 
+export const ordersCollection = collection(db, "orders");
 
-export const categoriesCollection = collection(db, 'categories');
-
-export const productsCollection = collection(db, 'products');
+const provider = new GoogleAuthProvider();
+export const signIn = () => signInWithPopup(auth, provider);
+export const signOff = () => signOut(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
