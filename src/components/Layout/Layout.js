605bf948a050ae7138bb0f1411289ec0b2355691
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-01 16:04:00 +0600

    added some elements to Layout.js for drawer to work

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,15 +4,28 @@ import CategoryList from "../CategoryList/CategoryList";
 import Logo from "./Logo/Logo";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
+import Drawer from "../Drawer/Drawer";
+import NavToggle from "../NavToggle/NavToggle";
+import { useState } from "react";
+
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
-        <CartLink />
-        <Auth />
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
+        <div>
+          <CartLink />
+          <Auth />
+        </div>
       </header>
       <aside>
         <CategoryList />
