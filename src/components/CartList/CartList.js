dfacca593220dbaf817155de07124f3d5aa034c2
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-12 14:03:55 +0600

    changed some things in CartLink to make the site work

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -1,47 +1,49 @@
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
+import "./CartList.css";
+import krestik from "../../assets/krestik.png";
 
 export default function CartList() {
-  // полуычить список товаров и корзинку
   const { products, cart, setCart } = useContext(AppContext);
 
   function onQuantityChange(product, qty) {
-    setCart ({
+    setCart({
       ...cart,
       [product.id]: qty,
-    })
+    });
   }
 
   function onItemRemove(product) {
     const newCart = { ...cart };
     delete newCart[product.id];
-    setCart(newCart); 
+    setCart(newCart);
   }
 
-  // получить только ИД товаров в корзинке
   const productIds = Object.keys(cart);
 
-  // чтобы вывести пройтись по товарам
   const output = products
-  // удалить все товары которые не в корзине
-  .filter((product) => productIds.includes(product.id))
-  // вывести товары и их количество   
-  .map((product) => (
-    <div className="CartItem" key={product.id}>
-      <img src={product.picture} alt={product.name} />
-      <Link to={"/product/" + product.slug}>{product.name}</Link>
-      {cart[product.id]}
-      <input type="number"
-       value={cart[product.id]}
-       min={1}
-       onChange={(event) => onQuantityChange
-       (product, +event.target.value)} />
-      <span>{cart[product.id] * product.price} som</span>
-      <button onClick={() => onItemRemove(product)}>Remove</button>
-    </div>
-  )) 
-  return (
-    <div className="CartList">{output}</div>
-  )
+    .filter((product) => productIds.includes(product.id))
+    .map((product) => (
+      <div className="CartItem" key={product.id}>
+        <div className="img-tovar">
+
+        </div>
+        <Link className="CartP" to={"/product/" + product.slug}>
+          {product.name}
+        </Link>
+        <input
+          type="number"
+          value={cart[product.id]}
+          min={1}
+          onChange={(event) => onQuantityChange(product, +event.target.value)}
+        />
+        <span>{(cart[product.id] * product.price).toFixed(2)} som</span>
+        <button onClick={() => onItemRemove(product)}>
+          <img className="krestik_img" src={krestik} alt="krestik" />
+        </button>
+      </div>
+    ));
+
+  return <div className="CartList">{output}</div>;
 }
