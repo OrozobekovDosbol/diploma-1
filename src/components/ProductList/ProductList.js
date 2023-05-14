0948e14d7d9d58bd4e93427a25b6af6b8b0ef382
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-15 01:57:08 +0600

    product images placed in a div and set the size

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -6,28 +6,31 @@ import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
 import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
-export default function ProductList( {category} ) {
+export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-  const output = products.filter(product => product.category === category.id)
-  .map(product => (
-    <div key={product.id} className="Product">
-      <a href={'/products/' + product.slug}>
-      <img src={product.picture} alt={product.name} width="200px" />
-      </a>
-      <NavLink to={'/products/' + product.slug}>
-        {product.name}
-      </NavLink>
-      <span><strong>Price:</strong>{product.price} som</span>
-      <AddToCart product={product} />
-      <DeleteProduct product={product} />
-    </div>
-  ))
+  const output = products
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div key={product.id} className="Product">
+        <div className="product-picture">
+          <a href={"/products/" + product.slug}>
+            <img src={product.picture} alt={product.name} />
+          </a>
+        </div>
+        <NavLink to={"/products/" + product.slug}>{product.name}</NavLink>
+        <span>
+          <strong>Price:</strong> ${product.price}
+        </span>
+        <AddToCart product={product} />
+        <DeleteProduct product={product} />
+      </div>
+    ));
   return (
     <div className="ProductList">
       {output}
 
       <AddProduct category={category} />
     </div>
-  )
+  );
 }
