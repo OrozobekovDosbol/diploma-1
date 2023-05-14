c2f71de73963341d9617735dee07b8e8ecf5afda
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-15 01:49:23 +0600

    in Add to Cart put a counter inside the button

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -15,8 +15,7 @@ export default function AddToCart({ product }) {
 
   return (
     <div className="AddToCart">
-      {cart[product.id] ? cart[product.id] : 0}
-      <button onClick={onAddToCartClick}>Add to cart</button>
+      <button onClick={onAddToCartClick}>{cart[product.id] ? cart[product.id] : 0}Add to cart</button>
     </div>
   );
 }
