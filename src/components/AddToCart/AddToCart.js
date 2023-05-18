1ab827b108764ed25dc7e5c64ca69ef708a33971
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-19 03:15:48 +0600

    restyled the AddToCart button

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -15,7 +15,7 @@ export default function AddToCart({ product }) {
 
   return (
     <div className="AddToCart">
-      <button onClick={onAddToCartClick}>{cart[product.id] ? cart[product.id] : 0}Add to cart</button>
+      <button onClick={onAddToCartClick}>{cart[product.id] ? cart[product.id] : 0} Add to cart</button>
     </div>
   );
 }
