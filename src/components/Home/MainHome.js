ff4293d86f3ab96043793214f3526322e0a78eb0
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-19 03:22:16 +0600

    added content for main page

--- src/components/Home/MainHome.js
+++ src/components/Home/MainHome.js
@@ -1,19 +1,22 @@
 import divan from "../../assets/divan.webp";
+import cushioned from "../../image/cushioned.jpg";
+import cushioned3 from "../../image/cushioned-3.jpg";
 import "./MainHome.css";
 import React from "react";
 export default function MainHome() {
   return (
     <div className="Home">
-      <div class="hover-text-one">
-        <figure class="effect-text-two">
-          <img src={divan} alt="divan" />
-          <figcaption>
-            <h3>welcome to our furniture store Dosbol</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-          </figcaption>
-        </figure>
-      </div>
       <h1>Furniture salon "Dos-Bol"</h1>
+      
+      <div className="header1">
+      <img className="dop-product header-image" src={divan} alt="Header background" />
+
+        <div className="header-text">
+          <h1 className="header-title">Welcome to our store</h1>
+          <p className="header-subtitle">you can buy home furniture from us</p>
+        </div>
+      </div>
+
       <p>
         When choosing furniture in Bishkek, in search of modern and reliable
         upholstered furniture or cabinet furniture, take a look at our furniture
@@ -26,6 +29,22 @@ export default function MainHome() {
         manufacturers. The furniture of which is distinguished by functionality
         and environmental safety.
       </p>
+      <div className="product-Home">
+        <a href="/products/willow-ii-slipcovered-bench-queen-sleeper-sofa">
+          <img
+            className="cushioned dop-product"
+            src={cushioned}
+            alt="cushioned"
+          />
+        </a>
+        <a href="/products/hague-mid-century-sofa">
+          <img
+            className="cushioned-3 dop-product"
+            src={cushioned3}
+            alt="cushioned-3"
+          />
+        </a>
+      </div>
     </div>
   );
 }
