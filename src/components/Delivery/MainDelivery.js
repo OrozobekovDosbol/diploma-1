717591c0c71ce7beb3f89fe670408aaf5cee74fd
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-19 03:19:48 +0600

    added content for Delivery and downloaded SVG

--- src/components/Delivery/MainDelivery.js
+++ src/components/Delivery/MainDelivery.js
@@ -1,7 +1,12 @@
 import "./MainDelivery.css";
+import Delivery from "../../image/furniture-delivery-1.svg";
 
 export default function MainDelivery() {
   return (
-    <p>rfjevu</p>
+    <div className="Delivery">
+      <h1>Transportation of furniture in Bishkek</h1>
+      <p>Gruzovichkof will come to you within 15 minutes after the call, help you disassemble bulky furniture, pack it, lower it and load it into the body. After transportation, movers will lift the furniture to the floor you need, unpack, assemble and put in place. For the transportation of furniture in Bishkek, a large selection of Gazelles with an open, soft and hard body is available. Door-to-door transportation services help you save time, money and effort.</p>
+      <img src={Delivery} alt="Delivery_photo" />
+    </div>
   )
 }
