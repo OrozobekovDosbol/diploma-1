733d0c191cc0d0d9b207f9dffa2c910cdc948cf2
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-19 14:26:57 +0600

    fixed bugs throughout the site

--- src/components/Contacts/MainContacts.js
+++ src/components/Contacts/MainContacts.js
@@ -92,7 +92,7 @@ export default function MainContacts() {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1105.3139412266441!2d78.38593421882258!3d42.48314339889575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38865b8a369e273f%3A0x4ab0c4529304e4bd!2sIssykkul%20Educational%20Advising%20Center!5e1!3m2!1sru!2skg!4v1684324715573!5m2!1sru!2skg"
         width="100%"
         height="450"
-        allowfullscreen=""
+        allowFullScreen=""
         loading="lazy"
         title="Embedded Google Maps"
         referrerpolicy="no-referrer-when-downgrade"
