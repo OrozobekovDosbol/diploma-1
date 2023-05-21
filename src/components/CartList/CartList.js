4caef5a96877b40c067df061ffd930f35154d5fa
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-21 20:12:15 +0600

    fixed bag in CartLict changed class to className

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -12,13 +12,13 @@ export default function CartList() {
       [product.id]: qty,
     });
   }
-
   function onItemRemove(product) {
     const newCart = { ...cart };
     delete newCart[product.id];
     setCart(newCart);
   }
 
+  // получить только ИД товаров в корзинке
   const productIds = Object.keys(cart);
 
   const output = products
@@ -48,7 +48,7 @@ export default function CartList() {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-x-lg"
+                className="bi bi-x-lg"
                 viewBox="0 0 16 16"
               >
                 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
