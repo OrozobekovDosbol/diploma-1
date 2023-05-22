c623cab694990177118f86eb44534fa8845cf9a7
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-22 22:19:55 +0600

    created a MainAbout folder and imported this folder into About.js which is inside the pages folder

---
+++ src/components/About/MainAbout.js
@@ -0,0 +1,32 @@
+import "./MainAbout.css";
+
+export default function MainAbout() {
+  return (
+    <div className="About">
+      <h1>Our features and benefits</h1>
+      <div className="AboutContent">
+        <div className="box-1">
+          <h1>1</h1>
+          <p>Large-scale production and technological resources</p>
+          <span>
+            Own production areas on the territory of more than 60,000 m2
+          </span> <br />
+          <span>Modern park of automated equipment</span> <br />
+          <span>
+            Own lamination line and the ability to work with exclusive decors
+          </span> <br />
+          <span>Innovative production processes</span> <br />
+        </div>
+        <div className="box-2">
+          <h1>2</h1>
+          <p>National Level Logistics</p>
+          <span>12 own regional offices total storage area over 50,000 m2</span> <br />
+          <span>Фutomated accounting, acceptance and shipment of products</span> <br />
+          <span>Вeliveries to all subjects of the Kyrgyz Republic</span> <br />
+          <span>Deliveries to the CIS countries</span> <br />
+          <span>The possibility of sending products by any mode of transport</span> <br />
+        </div>
+      </div>
+    </div>
+  )
+}
