4792e6e2633a56e3732299270f9cc20599f30a3c
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-22 14:06:07 +0600

    added style for OrderList and changed some things in it

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import "./OrderList.css";
+import { NavLink } from "react-router-dom";
 
 export default function OrderList() {
   const { orders, products } = useContext(AppContext);
@@ -9,36 +10,61 @@ export default function OrderList() {
     return "No orders found.";
   }
 
-  const output = orders.map(order => {
-    if (!order.cart) return "Empty"
-    const cartOutput = Object.keys(order.cart).map(productId => {
-      const product = products.find(product => product.id === productId);
+  const output = orders.map((order) => {
+    if (!order.cart) return "Empty";
+    const cartOutput = Object.keys(order.cart).map((productId) => {
+      const product = products.find((product) => product.id === productId);
 
       if (!product) {
-        return 'Product not found'
+        return "Product not found";
       }
 
       return (
-        <div key={product.id}>  
-          <img src={product.picture} alt={product.name} />
-          {product.name}: {order.cart[productId]} X {product.price} som = {order.cart[productId] * product.price} som
+        <div className="Order-content" key={product.id}>
+          <NavLink to={"/products/" + product.slug}>
+          <div className="Main-Order">
+            <div className="Order-picture">
+              <img src={product.picture} alt={product.name} />
+            </div>
+            <div className="Order-box-1">
+              <p>
+                <strong>Name: </strong>
+                {product.name}
+              </p>
+              <p>
+                <strong>Amount: </strong>
+                {order.cart[productId]}X
+              </p>
+              <p>
+                <strong>The price of the product: </strong>${product.price}
+              </p>
+              <p>
+                <strong>Total: </strong>${order.cart[productId] * product.price}
+              </p>
+            </div>
+          </div>
+          </NavLink>
         </div>
       );
-    })
+    });
 
     return (
       <div key={order.id} className="Order">
-        <div>Name: {order.name}</div>
-        <div>Phone: {order.phone}</div>
-        <div>Address: {order.address}</div>
-        <div>Cart: {cartOutput}</div>
+        <div className="order-p">
+          <strong>Name:</strong> {order.name}
+        </div>
+        <div className="order-p">
+          <strong>Phone: </strong> {order.phone}
+        </div>
+        <div className="order-p">
+          <strong>Address: </strong> {order.address}
+        </div>
+        <div className="order-Cart">
+          <strong>Cart: </strong> {cartOutput}
+        </div>
       </div>
     );
-  })
+  });
 
-  return (
-    <div className="OrderList">
-      {output}
-    </div>
-  );
+  return <div className="OrderList order-p">{output}</div>;
 }
