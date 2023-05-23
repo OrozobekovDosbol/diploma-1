9e2887594b4f4e0a9325ab27163d487dc10b7833
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-23 23:11:16 +0600

    added style and content in MainHome

--- src/components/Home/MainHome.js
+++ src/components/Home/MainHome.js
@@ -1,4 +1,4 @@
-import divan from "../../assets/divan.webp";
+import video_1 from "../../video/video-1.mp4";
 import cushioned from "../../image/cushioned.jpg";
 import cushioned3 from "../../image/cushioned-3.jpg";
 import "./MainHome.css";
@@ -6,38 +6,41 @@ import React from "react";
 export default function MainHome() {
   return (
     <div className="Home">
-      <h1>Furniture salon "Dos-Bol"</h1>
-      
-      <div className="header1">
-      <img className="dop-product header-image" src={divan} alt="Header background" />
+      <div className="video">
+        <video control autoPlay muted loop>
+          <source src={video_1} type="video/mp4" />
+        </video>
 
         <div className="header-text">
           <h1 className="header-title">Welcome to our store</h1>
-          <p className="header-subtitle">you can buy home furniture from us</p>
+          <p className="header-subtitle">You can buy home furniture from us</p>
         </div>
       </div>
 
-      <p>
-        When choosing furniture in Bishkek, in search of modern and reliable
-        upholstered furniture or cabinet furniture, take a look at our furniture
-        store in Karakol. We will be glad to see you in our furniture showroom
-        at the address: KR, Karakol, highway - Isa Kuchukova at the intersection
-        of Tynystanov. Furniture salon "Dos-Bol". Our showroom presents Polish,
-        Russian and Belarusian furniture manufacturers. All factories with which
-        we work have proven themselves in the markets of Eastern and Western
-        Europe. We select suppliers who have established themselves as reliable
-        manufacturers. The furniture of which is distinguished by functionality
-        and environmental safety.
-      </p>
+      <div className="box-2">
+        <h1>Furniture salon "Dosbol"</h1>
+        <p>
+          We are your reliable partner in a cozy and stylish environment. In our
+          store you will find a wide selection of quality furniture for all
+          areas of your home or office.
+        </p>
+        <p>
+          Our goal is to help you realize your dreams of the perfect space. We
+          offer a variety of styles and designs to suit different tastes and
+          preferences. Whether you are looking for modern minimalist furniture,
+          classic elegant style or something unique and original, we have what
+          you need.
+        </p>
+      </div>
       <div className="product-Home">
-        <a href="/products/willow-ii-slipcovered-bench-queen-sleeper-sofa">
+        <a className="product-a cush-1" href="/products/willow-ii-slipcovered-bench-queen-sleeper-sofa">
           <img
             className="cushioned dop-product"
             src={cushioned}
             alt="cushioned"
           />
         </a>
-        <a href="/products/hague-mid-century-sofa">
+        <a className="product-a cush-3" href="/products/hague-mid-century-sofa">
           <img
             className="cushioned-3 dop-product"
             src={cushioned3}
