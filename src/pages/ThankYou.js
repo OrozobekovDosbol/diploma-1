2865d6d53450036128938e59f433f8b92d5e566d
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-24 22:39:41 +0600

    changed photo Thank you

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -4,16 +4,16 @@ export default function ThankYou() {
   }, 2000);
   return (
     <div className="ThankYou">
+      <img
+        src="https://cdn.pixabay.com/photo/2017/12/08/23/44/thank-you-3006975_960_720.png"
+        alt="dsds"
+        width={250}
+      />
       <h1>Thank you!</h1>
       <p>
         Your order has been received. Our specialists will contact with you
         shortly to confirm it.
       </p>
-      <img
-        src="https://www.funimada.com/assets/images/cards/big/thank-you-2.gif"
-        alt="dsds"
-        width={250}
-      />
     </div>
   );
 }
