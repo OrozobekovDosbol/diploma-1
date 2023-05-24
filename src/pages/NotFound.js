0a71e9a8d5eb0e59b55e8ef2be485c6022604078
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-05-24 21:25:21 +0600

    changed page content Not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <div className="NotFound">
-      <h1>404</h1>
-      <h2>Everyone left to eat ashlyam-fu</h2>
+      <h1>404 Page not found</h1>
+      <h2>Sorry, we cannot find the page you are looking for. Please check</h2>
       <img
         src="https://i.gifer.com/origin/6f/6fc964b518401f22cc59707a8c9ca6ee_w200.gif"
         alt="error"
